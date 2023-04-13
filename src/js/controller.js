ef433c87c356535682bc7113bb78e06b5e349562
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-04-13 17:23:36 +0200

    Loading a recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,5 +9,29 @@ const timeout = function (s) {
 };
 
 // https://forkify-api.herokuapp.com/v2
+// prettier-ignore
+const showRecipe = async function () {
+  try {
+    const res = 
+    await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=293b9025-faa1-484a-a6a2-8739bf71a7e5');
 
-///////////////////////////////////////
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${data.status})`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredints: recipe.ingredints,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
